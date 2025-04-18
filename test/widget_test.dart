9de1d1579014d438c1b3fdf71468fba6a6846e6c
ebuka <ekpewerechiebuka@gmail.com>
2025-04-18 21:21:59 +0100

    fixes

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,31 +1,27 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:vesti_assessment_app/presentation/pages/auth/onboard_screen.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:vesti_assessment_app/presentation/pages/dashboard/home_page.dart';
 
 void main() {
-  testWidgets('OnboardScreen renders with title, subtitle, and Get Started button', (WidgetTester tester) async {
+  testWidgets('HomePage displays sections and book lists', (WidgetTester tester) async {
     await tester.pumpWidget(
-       ProviderScope(
+      ProviderScope(
         child: MaterialApp(
-          home: OnboardScreen(),
+          home: HomePage(),
         ),
       ),
     );
 
     await tester.pumpAndSettle();
 
-    expect(find.byType(Image), findsWidgets);
-
-
-    expect(find.text('Enjoy the Best Features'), findsOneWidget);
-    
-    expect(
-      find.text('Experience the magic of requesting for books anytime,anywhere'),
-      findsOneWidget,
-    );
-
-
-    expect(find.text('Get Started'), findsOneWidget);
+    expect(find.text('Welcome Back!'), findsOneWidget);
+    expect(find.text('Mark Edwards'), findsOneWidget);
+    expect(find.text('Category'), findsOneWidget);
+    expect(find.text('Recently Borrowed'), findsOneWidget);
+    expect(find.text('New Arrival'), findsOneWidget);
+    expect(find.text('Recommended For You'), findsOneWidget);
+    expect(find.byType(ListView), findsNWidgets(3));
+    expect(find.byType(GestureDetector), findsWidgets);
   });
 }
